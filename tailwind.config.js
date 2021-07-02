6af743b63af0eb3f5558b32140f9b5b754c92881
Javier Benavides <chato.javier@gmail.com>
2021-07-01 23:33:07 -0500

    landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,35 @@
 module.exports = {
-  purge: ['./dist/*.html'],
+  purge: [
+    // Paths to your templates...
+    "./**.html",
+    "./src/js/**.js"
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    maxWidth: {
+      '1280': '1280px',
+      '1024': '1024px'
+    },
+    colors: {
+      red: '#ff2a00',
+      black: '#000',
+      white: '#fff'
+    },
+    fontFamily: {
+    },
+    extend: {
+      height: {
+        '125': '31.25rem'
+      },
+      borderWidth: {
+        '1': '1px'
+      }
+    }
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    
+  ],
 }
